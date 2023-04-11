82506d42a93e772ef99763ea07f384a42bc04a05
tinld <19522348@gm.uit.edu.vn>
2023-04-11 10:01:46 +0700

    change version type setup

--- proton/setup.sh
+++ proton/setup.sh
@@ -1,6 +1,3 @@
-conda create -n text2sql python=3.6
-source activate text2sql
-pip install torch==1.6.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 python -c "import stanza; stanza.download('en')"
 python -c "from embeddings import GloveEmbedding; emb = GloveEmbedding('common_crawl_48', d_emb=300)"
