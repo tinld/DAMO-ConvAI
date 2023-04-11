14eb73e6bebf892ea502fbac49d6b9c13442a382
tinld <19522348@gm.uit.edu.vn>
2023-04-11 23:26:17 +0700

    change devbin to devjson

--- proton/run/run_preprocessing.sh
+++ proton/run/run_preprocessing.sh
@@ -12,9 +12,9 @@ vocab='pretrained_models/glove.42b.300d/vocab.txt'
 #echo "Start to preprocess the original train dataset ..."
 #python3 -u preprocess/process_dataset.py --dataset_path ${train_data} --raw_table_path ${table_data} --table_path ${table_out} --output_path 'data/train.bin' --skip_large #--verbose > train.log
 echo "Start to preprocess the original dev dataset ..."
-python3 -u preprocess/process_dataset.py --dataset_path ${dev_data} --raw_table_path ${table_data} --table_path ${table_out} --output_path 'data/dev.bin' #--verbose > dev.log
+python3 -u preprocess/process_dataset.py --dataset_path ${dev_data} --raw_table_path ${table_data} --table_path ${table_out} --output_path 'data/dev.json' #--verbose > dev.log
 #echo "Start to build word vocab for the dataset ..."
 #python3 -u preprocess/build_glove_vocab.py --data_paths 'data/train.bin' --table_path ${table_out} --reference_file ${vocab_glove} --mwf 4 --output_path ${vocab}
 echo "Start to construct graphs for the dataset ..."
 #python3 -u preprocess/process_graphs.py --dataset_path 'data/train.bin' --table_path ${table_out} --method 'lgesql' --output_path ${train_out}
-python3 -u preprocess/process_graphs.py --dataset_path 'data/dev.bin' --table_path ${table_out} --method 'lgesql' --output_path ${dev_out}
+python3 -u preprocess/process_graphs.py --dataset_path 'data/dev.json' --table_path ${table_out} --method 'lgesql' --output_path ${dev_out}
