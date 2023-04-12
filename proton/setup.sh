d6c4a23558d872ea336686f3e5f42bae6ddb8c3a
tinld <19522348@gm.uit.edu.vn>
2023-04-12 12:59:03 +0700

    update version for proton

--- proton/setup.sh
+++ proton/setup.sh
@@ -1,3 +1,4 @@
+pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 python -c "import stanza; stanza.download('en')"
 python -c "from embeddings import GloveEmbedding; emb = GloveEmbedding('common_crawl_48', d_emb=300)"
